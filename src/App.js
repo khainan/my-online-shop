2539dd792a419562538a9ef1d629222bf2299875
khainan <muhammadrachim.k@gmail.com>
2020-08-23 17:04:03 +0700

    add carousel

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import './main.scss'
 import Navbar from "./component/navbar";
+import CarouselComponent from './component/Carousel';
 
 function App() {
   return (
     <div className="talia-portfolio-app">
       <Navbar />
+      <CarouselComponent />
     </div>
   );
 }
