9157de39b3a91aa1e2013fb4c8e858c69bb8b039
khainan <muhammadrachim.k@gmail.com>
2020-08-23 17:09:22 +0700

    fix carousel

--- src/component/Carousel.js
+++ src/component/Carousel.js
@@ -8,7 +8,7 @@ function CarouselComponent() {
     <Carousel>
       <Carousel.Item>
         <img
-          className="d-block w-100 h-100"
+          className="d-block w-100"
           src={img1}
           alt="First slide"
         />
@@ -19,7 +19,7 @@ function CarouselComponent() {
       </Carousel.Item>
       <Carousel.Item>
         <img
-          className="d-block w-100 h-100"
+          className="d-block w-100"
           src={img2}
           alt="Third slide"
         />
